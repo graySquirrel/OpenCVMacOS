d65642efa56286f8665511e420d35461dd3e27ce
Fritz Ebner <fritzebner@patmac.local>
2019-04-03 13:23:19 -0400

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Fritz Ebner on 4/3/19.
+//  Copyright © 2019 Fritz Ebner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
